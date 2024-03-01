4e16716b51c411dcec89c7052ea06c2e60444a62
Abraham Miguel <amiguel@unal.edu.co>
2024-02-29 19:42:07 -0500

    primera entrega

--- preguntas.py
+++ preguntas.py
@@ -11,21 +11,31 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
-
-
-def pregunta_01():
-    """
+"""
     Retorne la suma de la segunda columna.
 
     Rta/
     214
 
     """
-    return
+
+def pregunta_01():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
+
+    sum = 0
+    for row in relacion:
+        sum += int(row[1])
+
+    return sum
+
+print("\nPregunta 1")
+print(pregunta_01())
 
 
-def pregunta_02():
-    """
+
+"""
     Retorne la cantidad de registros por cada letra de la primera columna como la lista
     de tuplas (letra, cantidad), ordendas alfabéticamente.
 
@@ -39,11 +49,29 @@ def pregunta_02():
     ]
 
     """
-    return
+
+def pregunta_02():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
+
+    dic = {}
+    for fila in relacion:
+        if fila[0] not in dic.keys():
+            dic[fila[0]] = 1
+        else:
+            dic[fila[0]] +=1
+
+        tuplas = list(dic.items())
+        tuplas.sort(key=lambda x: x[0])
+
+    return tuplas
+
+print("\nPregunta 2")
+print(pregunta_02())
 
 
-def pregunta_03():
-    """
+"""
     Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
     de tuplas (letra, suma) ordendas alfabeticamente.
 
@@ -57,11 +85,27 @@ def pregunta_03():
     ]
 
     """
-    return
+def pregunta_03():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
+    dic = {}
+    for fila in relacion:
+        if fila[0] not in dic.keys():
+            dic[fila[0]] = int(fila[1])
+        else:
+            dic[fila[0]] += int(fila[1])
 
-def pregunta_04():
-    """
+        tuplas = list(dic.items())
+        tuplas.sort(key=lambda x: x[0])
+
+    return tuplas
+
+print("\nPregunta 3")
+print(pregunta_03())
+
+"""
     La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
     registros por cada mes, tal como se muestra a continuación.
 
@@ -82,11 +126,29 @@ def pregunta_04():
     ]
 
     """
-    return
+def pregunta_04():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
+
+    dic = {}
+    for fila in relacion:
+        if fila[2][5:7] not in dic.keys():
+            dic[fila[2][5:7]] = 1
+        else: 
+            dic[fila[2][5:7]] +=1
+
+    tuplas = list(dic.items())
+    tuplas.sort(key=lambda x:x[0])
+
+    return tuplas
+
+print("\nPregunta 4")
+print(pregunta_04())
 
 
-def pregunta_05():
-    """
+
+"""
     Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
     letra de la columa 1.
 
@@ -100,11 +162,27 @@ def pregunta_05():
     ]
 
     """
-    return
+def pregunta_05():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
+    dic = {}
+    for fila in relacion:
+        if fila[0] not in dic.keys():
+            dic[fila[0]] = []
+            dic[fila[0]].append(int(fila[1]))
+        else:
+            dic[fila[0]].append(int(fila[1]))
 
-def pregunta_06():
-    """
+    tuplas = [(key, max(values), min(values)) for key, values in dic.items()]
+    tuplas.sort(key=lambda x: x[0])
+    return tuplas
+
+print("\nPregunta 5")
+print(pregunta_05())
+
+"""
     La columna 5 codifica un diccionario donde cada cadena de tres letras corresponde a
     una clave y el valor despues del caracter `:` corresponde al valor asociado a la
     clave. Por cada clave, obtenga el valor asociado mas pequeño y el valor asociado mas
@@ -125,11 +203,31 @@ def pregunta_06():
     ]
 
     """
-    return
+def pregunta_06():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
+    dic = {}
+    for fila in relacion:
+        fila[-1] = fila[-1].rstrip("\n")  # Eliminar el salto de línea al final
+        separados = fila[-1].split(",")
 
-def pregunta_07():
-    """
+        for elemento in separados:
+            if elemento[0:3] not in dic.keys():
+                dic[elemento[0:3]] = []
+                dic[elemento[0:3]].append(int(elemento[4:]))
+            else: 
+                dic[elemento[0:3]].append(int(elemento[4:]))
+
+        tuplas = [(key, min(values), max(values)) for key, values in dic.items()]
+        tuplas.sort(key=lambda x: x[0])
+    return tuplas
+
+print("\nPregunta 6")
+print(pregunta_06())
+
+"""
     Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
     valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
     a dicho valor de la columna 2.
@@ -149,11 +247,28 @@ def pregunta_07():
     ]
 
     """
-    return
+def pregunta_07():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
+    dic = {}
+    for fila in relacion:
+        if fila[1] not in dic.keys():
+            dic[fila[1]] = []
+            dic[fila[1]].append(fila[0])
+        else:
+            dic[fila[1]].append(fila[0])
 
-def pregunta_08():
-    """
+    tuplas = list(dic.items())
+    tuplas.sort(key=lambda x:x[0])
+
+    return tuplas
+
+print("\nPregunta 7")
+print(pregunta_07())
+
+"""
     Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
     de la segunda columna; la segunda parte de la tupla es una lista con las letras
     (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
@@ -174,11 +289,30 @@ def pregunta_08():
     ]
 
     """
-    return
 
+def pregunta_08():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
-def pregunta_09():
-    """
+    dic = {}
+    for fila in relacion:
+        if int(fila[1]) not in dic.keys():
+            dic[int(fila[1])] = []
+            dic[int(fila[1])].append(fila[0])
+        elif fila[0] not in dic[int(fila[1])]:
+            dic[int(fila[1])].append(fila[0])
+
+    tuplas = [(key, sorted(values)) for key, values in dic.items()]
+    tuplas.sort(key=lambda x: x[0])
+    
+
+    return tuplas
+
+print("\nPregunta 8")
+print(pregunta_08())
+
+"""
     Retorne un diccionario que contenga la cantidad de registros en que aparece cada
     clave de la columna 5.
 
@@ -197,11 +331,30 @@ def pregunta_09():
     }
 
     """
-    return
 
+def pregunta_09():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
-def pregunta_10():
-    """
+    dic = {}
+    for fila in relacion:
+        fila[-1] = fila[-1].rstrip("\n")
+        separados = fila[-1].split(",")
+
+        for elemento in separados:
+            if elemento[0:3] not in dic.keys():
+                dic[elemento[0:3]] = 1
+            else:
+                dic[elemento[0:3]] +=1
+    dic = dict(sorted(dic.items(), key=lambda x: x[0]))
+
+    return dic
+
+print("\nPregunta 9")
+print(pregunta_09())
+
+"""
     Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
     cantidad de elementos de las columnas 4 y 5.
 
@@ -218,11 +371,22 @@ def pregunta_10():
 
 
     """
-    return
 
+def pregunta_10():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
-def pregunta_11():
-    """
+    tuplas = []
+    for fila in relacion:
+        tuplas.append((fila[0], len(fila[-2].split(",")), len(fila[-1].split(","))))
+
+    return tuplas
+
+print("\nPregunta 10")
+print(pregunta_10())
+
+"""
     Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
     columna 4, ordenadas alfabeticamente.
 
@@ -239,11 +403,30 @@ def pregunta_11():
 
 
     """
-    return
 
+def pregunta_11():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
 
-def pregunta_12():
-    """
+    dic = {}
+    for fila in relacion:
+        separados = fila[-2].split(",")
+
+        for letra in separados:
+            if letra not in dic.keys():
+                dic[letra] = int(fila[1])
+            else:
+                dic[letra] += int(fila[1])
+
+    dic = dict(sorted(dic.items(), key=lambda x: x[0]))
+
+    return dic
+
+print("\nPregunta 11")
+print(pregunta_11())
+
+"""
     Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
     los valores de la columna 5 sobre todo el archivo.
 
@@ -257,4 +440,25 @@ def pregunta_12():
     }
 
     """
-    return
+
+def pregunta_12():
+    with open("data.csv", "r") as file:
+        relacion = file.readlines()
+        relacion = [line.split("\t") for line in relacion]
+
+    dic = {}
+    for fila in relacion:
+        fila[-1] = fila[-1].rstrip("\n")
+        separados = fila[-1].split(",")
+
+        for elemento in separados:
+            if fila[0] not in dic.keys():
+                dic[fila[0] ] = int(elemento[4:])
+            else:
+                dic[fila[0] ] += int(elemento[4:])
+
+    dic = dict(sorted(dic.items(), key=lambda x: x[0]))
+    return dic
+
+print("\nPregunta 12")
+print(pregunta_12())
